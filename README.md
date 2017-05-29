4a7260aec21382e68877bcf7fd3ba6194000ee64
Adrián González Rus <adrian.gonzalez.rus@bbva.com>
2017-05-29 09:14:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## ember-cli-qunit-parameterize
+
+This addon that adds `qunit-parameterize` to the generated Ember CLI output (in `test-support.js`).
+
+### Installation / Usage
+
+From within your Ember CLI application (must be > 0.0.34), run the following:
+
+```bash
+ember install ember-cli-qunit-parameterize
+```
+
+### References
+
+* [qunit-parameterize](https://github.com/AStepaniuk/qunit-parameterize)
---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var debug = require('debug')('broccoli-stew:map');
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
+var map = require('broccoli-stew').map;
+
+module.exports = {
+  name: 'Ember CLI qunit-parameterize',
+
+  treeForAddonTestSupport: function() {
+    var path = require.resolve('qunit-parameterize/qunit2-parameterize').replace('qunit2-parameterize.js', '');
+    var mapped =  map(path, 'qunit2-parameterize.js', function(content, relativePath) {
+      var newContent = [
+        ";define('qunit-parameterize', ['exports', 'ember-qunit'], function(exports, emberQunit) {\n",
+        "var QUnit = window.QUnit; emberQunit.extend = QUnit.extend; QUnit = emberQunit;\n",
+        content,
+        "exports['default'] = QUnit.cases.init.bind(QUnit.cases);\n});"
+      ];
+
+      return newContent.join('');
+    });
+
+    return new Funnel(mapped, {
+      include: ['qunit2-parameterize.js']
+    });
+  }
+
+};
