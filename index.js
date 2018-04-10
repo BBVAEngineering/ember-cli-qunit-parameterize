b217aa3dea2af3042aa6b93c972c4a6c3f3781c1
Eduardo Tarancon Delgado <eduardo.tarancon.contractor@bbva.com>
2018-04-10 20:05:41 +0200

    fix linter

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 'use strict';
 
-var Funnel = require('broccoli-funnel');
-var map = require('broccoli-stew').map;
+const Funnel = require('broccoli-funnel');
+const map = require('broccoli-stew').map;
 
 module.exports = {
-  name: 'Ember CLI qunit-parameterize',
+	name: 'Ember CLI qunit-parameterize',
 
-  treeForAddonTestSupport: function() {
-    var path = require.resolve('qunit-parameterize/qunit2-parameterize').replace('qunit2-parameterize.js', '');
-    var mapped =  map(path, 'qunit2-parameterize.js', function(content, relativePath) {
-      var newContent = [
-        ";define('qunit-parameterize', ['exports', 'ember-qunit'], function(exports, emberQunit) {\n",
-        "var QUnit = window.QUnit; emberQunit.extend = QUnit.extend; QUnit = emberQunit;\n",
-        content,
-        "exports['default'] = QUnit.cases.init.bind(QUnit.cases);\n});"
-      ];
+	treeForAddonTestSupport: () => {
+		const path = require.resolve('qunit-parameterize/qunit2-parameterize').replace('qunit2-parameterize.js', '');
+		const mapped = map(path, 'qunit2-parameterize.js', (content) => {
+			const newContent = [
+				";define('qunit-parameterize', ['exports', 'ember-qunit'], function(exports, emberQunit) {\n",
+				'var QUnit = window.QUnit; emberQunit.extend = QUnit.extend; QUnit = emberQunit;\n',
+				content,
+				"exports['default'] = QUnit.cases.init.bind(QUnit.cases);\n});"
+			];
 
-      return newContent.join('');
-    });
+			return newContent.join('');
+		});
 
-    return new Funnel(mapped, {
-      include: ['qunit2-parameterize.js']
-    });
-  }
-
+		return new Funnel(mapped, {
+			include: ['qunit2-parameterize.js']
+		});
+	}
 };
